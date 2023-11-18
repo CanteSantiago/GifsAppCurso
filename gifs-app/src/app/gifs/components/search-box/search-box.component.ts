405396e262decb590faece1558a8534d9e1af782
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-18 17:39:36 -0500

    Vid90-@Viewchild
    Usar viewChild para tomar una referencia local

--- gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
@@ -6,10 +6,23 @@ import { Component, Input, OnInit } from '@angular/core';
  <h5>Buscar:</h5>
  <input type="text"
  class="form-control"
- placeholder="Buscar gifs...">
+ placeholder="Buscar gifs..."
+(keyup.enter)="searchTag()"
+#txtTagInput
+ >
  `
 })
 export class SearchBoxComponent  {
 
+  @ViewChild('txtTagInput')
+  public tagInput!:ElementRef <HTMLInputElement>;
+
+constructor(){ }
+
+  // searchTag(newTag: string){
+    searchTag(){
+      const newTag = this.tagInput.nativeElement.value;
+    console.log({newTag});
+  }
 
 }
