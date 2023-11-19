387f0430abf3179c480337c3146d6aa636d2e8d8
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-18 23:43:31 -0500

    Vid107-LazyImage1

---
+++ gifs-app/src/app/shared/components/lazyImage/lazyImage.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazyImage.component.html',
+})
+export class LazyImageComponent  {
+
+  constructor() { }
+
+
+
+}
