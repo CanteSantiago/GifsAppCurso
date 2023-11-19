ba21e8253398c865b8d9e407796663b00eca0570
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-18 22:13:35 -0500

    Vid99-PersistenciaLocal

--- gifs-app/src/app/gifs/services/gifs.service.ts
+++ gifs-app/src/app/gifs/services/gifs.service.ts
@@ -25,8 +25,11 @@ private serviceUrl:string='https://api.giphy.com/v1/gifs';
 
     this._tagsHistory.unshift(tag);
 this._tagsHistory=this.tagsHistory.splice(0,10);
+this.saveLocalStorage();
   }
-
+private saveLocalStorage():void{
+  localStorage.setItem('history',JSON.stringify(this._tagsHistory));
+}
 
    searchTag (tag:string):void{
 if (tag.length===0) return;
