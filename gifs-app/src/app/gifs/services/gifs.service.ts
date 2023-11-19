19ef99aca42a294a82fec61606c201145931d6fc
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-18 23:12:56 -0500

    Vid100-LeerDelLocalStorage

--- gifs-app/src/app/gifs/services/gifs.service.ts
+++ gifs-app/src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,10 @@ private _tagsHistory: string[]=[];
 private apiKey:string='fkEwBDXKIlX5TkwpMxBkM2srWWcJgvmv';
 private serviceUrl:string='https://api.giphy.com/v1/gifs';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    this.loadLocalStorage();
+    console.log('Gifs service ready');
+  }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -29,6 +32,16 @@ this.saveLocalStorage();
   }
 private saveLocalStorage():void{
   localStorage.setItem('history',JSON.stringify(this._tagsHistory));
+}
+
+private loadLocalStorage():void{
+  if(!localStorage.getItem('history'))return;
+
+  this._tagsHistory= JSON.parse(localStorage.getItem('history')!);
+
+  if (this._tagsHistory.length===0)return;
+  this.searchTag(this._tagsHistory[0]);
+
 }
 
    searchTag (tag:string):void{
